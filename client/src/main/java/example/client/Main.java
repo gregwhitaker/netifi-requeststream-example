bed870722b81aa0ed82140ffe7f5fd0a49734ccf
gregwhitaker <gwhitake@gmail.com>
2019-11-10 12:20:23 -0800

    Doing work

--- client/src/main/java/example/client/Main.java
+++ client/src/main/java/example/client/Main.java
@@ -1,5 +1,6 @@
 package example.client;
 
+import com.netifi.broker.BrokerClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,18 @@ public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String... args) {
+        // Build Netifi Broker Connection
+        BrokerClient netifi =
+                BrokerClient.tcp()
+                        .group("example.client")                        // Group name of client
+                        .destination("client1")                         // Name of this client instance
+                        .accessKey(9007199254740991L)
+                        .accessToken("kTBDVtfRBO4tHOnZzSyY5ym2kfY=")
+                        .host("localhost")                              // Netifi Broker Host
+                        .port(8001)                                     // Netifi Broker Port
+                        .disableSsl()
+                        .build();
 
+        
     }
 }
