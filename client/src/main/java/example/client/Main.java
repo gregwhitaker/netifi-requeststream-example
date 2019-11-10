d435fa8dc2e381cc1d900e903f22b2d844864d3b
gregwhitaker <gwhitake@gmail.com>
2019-11-10 11:26:13 -0800

    Doing work

--- client/src/main/java/example/client/Main.java
+++ client/src/main/java/example/client/Main.java
@@ -1,6 +1,10 @@
 package example.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String... args) {
 
